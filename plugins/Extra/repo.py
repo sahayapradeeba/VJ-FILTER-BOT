2e3fe6beca31c412f28f4d5862665b984f197955
Sahaya Pradeeba T <131452422+sahayapradeeba@users.noreply.github.com>
2024-06-06 11:55:36 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -35,7 +35,7 @@ async def git(bot, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @VJ_Botz
+<b>Powered by :</b> @Sky_HD_Movies
 
 """
         if qw.get("description"):
